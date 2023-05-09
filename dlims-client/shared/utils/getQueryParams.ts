f768ebd0079dbc8d0b612c8b14924890731982c5
yousafsabir <myousafdev@gmail.com>
2023-05-09 23:53:46 +0500

    feat: get query params util function

---
+++ dlims-client/shared/utils/getQueryParams.ts
@@ -0,0 +1,17 @@
+function getQueryParams() {
+    var pairs = window.location.search.substring(1).split("&"),
+        obj: any = {},
+        pair,
+        i;
+
+    for (i in pairs) {
+        if (pairs[i] === "") continue;
+
+        pair = pairs[i].split("=");
+        obj[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+    }
+
+    return obj;
+}
+
+export default getQueryParams
